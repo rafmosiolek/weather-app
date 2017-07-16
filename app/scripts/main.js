652ed6f03754dfe173744d55eb5b8da288e61ef9
RafM <rkjmosiolek@gmail.com>
2017-07-16 23:14:10 +0100

    add temp and precip statements to giveTip function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -66,70 +66,57 @@ function getDarkskyData(url) {
 		dataType: "jsonp",
 		url: url,
 		success: function(data) {
-			$("#weather-data .temperature").html("<h2>" + data.currently.temperature + "°C</h2>");
-			console.log(data.currently.temperature);
-			console.log(data.daily.data[0].temperatureMax);
-			console.log(data.daily.data[0].temperatureMin);
+			$("#weather-data .temperature").html("<h2>" + (Math.round(data.currently.temperature).toFixed(1)) + "°C</h2>");
+			console.log("Current temp: " + data.currently.temperature);
+			console.log("High temp: " + data.daily.data[0].temperatureMax);
+			console.log("Min temp: " + data.daily.data[0].temperatureMin);
 			console.log(data.daily.data[0].icon);
 			$("#weather-details .humidity").html("<p>Humidity: " + data.currently.humidity * 100 + "%</p>");
-			console.log(data.currently.humidity);
+			console.log("Humidity: " + data.currently.humidity);
 			$("#weather-details .wind").html("<p>Wind: " + data.currently.windSpeed + " KPH</p>");
-			console.log(data.currently.windSpeed);
+			console.log("Wind speed: " + data.currently.windSpeed);
 
 			console.log(data.hourly.data[0].precipProbability);
-
-			giveTip(data.daily.data[0].icon);
+			// pass the temperature for the next hour and precipitation probability as giveTip function's parameters
+			giveTip(data.hourly.data[0].temperature, data.hourly.data[0].precipProbability);
+			// pass the icon as a setBackground's parameter
 			setBackground(data.daily.data[0].icon);
 		}
 	});
 }
 
 
-function giveTip(icon) {
-	console.log(icon);
-	switch (icon) {
-		case "clear-day":
-			$("#clothing-tips .tips").html("<p>T-shirt weather! Don't forget your coolest shades!</p>");
-			break;
-		case "clear-day":
-			$("#clothing-tips .tips").html("<p>T-shirt weather!, but it might rain so take your brolly!</p>");
-			break;
-		case "clear-night":
-			$("#clothing-tips .tips").html("<p>Still perfect for t-shirt, but, you know, it's night, so maybe a jumper?</p>");
-			break;
-		case "clear-night":
-			$("#clothing-tips .tips").html("<p>You better wear your rain coat!</p>");
-			break;
-		case "rain":
-			$("#clothing-tips .tips").html("<p>Don't forget your umbrella!</p>");
-			break;
-		case "snow":
-			$("#clothing-tips .tips").html("<p>WINTER IS COMING! But seriously, wear some warm undies, will you?</p>");
-			break;
-		case "sleet":
-			$("#clothing-tips .tips").html("<p>Stay at home. But if you have to leave, take your best rain coat.</p>");
-			break;
-		case "wind":
-			$("#clothing-tips .tips").html("<p>It's windy out there! Some warm jumper comes in handy!</p>");
-			break;
-		case "fog":
-			$("#clothing-tips .tips").html("<p></p>");
-			break;
-		case "cloudy":
-			$("#clothing-tips .tips").html("<p></p>");
-			break;
-		case "partly-cloudy-day":
-			$("#clothing-tips .tips").html("<p>It's quite warm outside, but just in case take a longsleeve with you!</p>");
-			break;
-		case "partly-cloudy-night":
-			$("#clothing-tips .tips").html("<p></p>");
-			break;
-		default:
-			break;
+function giveTip(temp, precip) {
+	console.log(temp, precip);
+
+	var tip = "";
+	// statements for temperature
+	if (temp >= 20) {
+		tip += "It's a T-shirt weather! ";
+	} else if (temp >= 10) {
+		tip += "It's a sweater weather! ";
+	} else if (temp >= 0){
+		tip += "Wear warm undies! ";
+	} else {
+		tip += "It's freezing outside! Layer up! ";
 	}
+
+	// statements for precipitation
+	if (precip >= 0.6) {
+		tip += "And it is going to rain, don't forget your brolly!";
+	} else if (precip >= 0.3) {
+		tip += "And it might rain, you better take your rain coat!";
+	} else {
+		tip += "No need for umbrella today, yaaay!";
+	}
+	
+	$("#clothing-tips .tips").html("<p>" + tip + "</p>");
+
 }
 
 
+
+// $("#clothing-tips .tips").html("<p></p>");
 function setBackground(icon) {
 	console.log(icon);
 	switch (icon) {
