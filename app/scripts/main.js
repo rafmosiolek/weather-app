af1160ce6521aad8cc85bc8ef67b39904201519c
RafM <rkjmosiolek@gmail.com>
2017-07-17 00:47:27 +0100

    add comments to the js file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,7 @@ function userGeoLocation() {
 	// use the Geolocation object to obtain position of user's device
 	// check if the user's browser supports geolocation API
 	if (navigator.geolocation) {
+		// use getCurrentPosition method to get the current position of user's device, set the timout for 10s
 		navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {timeout:10000});
 	}
 }
@@ -23,6 +24,7 @@ function successCallback(position) {
 }
 
 function errorCallback(error) {
+	// append following error to the document if navigator.geolocation won't work
 	$("#error").html("<p>Geolocation is not supported by this browser! <br />" + errorMsg + "</p>");
 	var errorMsg = "Unknown error";
 	switch(error.code) {
@@ -41,14 +43,17 @@ function errorCallback(error) {
 }
 
 function displayCity(latitude, longitude) {
-	// use Google Maps Geocoding API to get the user's location by latitude and longitude look up
+	// use Google Maps Geocoding API and reverse geocode geographic coordinates into a human-readable address
 	$.ajax({
 		format: "json",
 		dataType: "json",
+		// restrict a Google Map API key with the most original string you could come up with
 		url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + ", " + longitude + "&sensor=true?key=madebymanyrules",
 		success: function(data) {
 			$("#location .row").html("<h2>" + data.results[0].address_components[1].long_name + ", " + data.results[0].address_components[4].long_name + "</h2>");
+			// log the district/area name
 			console.log(data.results[0].address_components[1].long_name);
+			// log the city/town name
 			console.log(data.results[0].address_components[4].long_name);
 		},
 		method: "GET"
@@ -56,25 +61,31 @@ function displayCity(latitude, longitude) {
 }
 
 function fireDarkskyApi(latitude, longitude) {
+	// add HTTP query parameter at the end of the url to use SI units
 	var url = "https://api.darksky.net/forecast/c78e6b910ba14ef6ac41cfc1e46b3af5/" + latitude + "," + longitude + "?units=si";
 	console.log(url);
+	// call the getDarkskyData function passing the url with user's device coordinates as a parameter
 	getDarkskyData(url);
 }
 
 
 function getDarkskyData(url) {
+	// I probably should have made a CORS request here, but I am not really familiar with the concept, hence the good old JSONP
 	$.ajax({
 		format: "jsonp",
 		dataType: "jsonp",
 		url: url,
 		success: function(data) {
+			// round the current temperature at user's location to the first decimal place and append it to the document
 			$("#weather-data .temperature").html("<h2>" + (Math.round(data.currently.temperature).toFixed(1)) + "°C</h2>");
 			console.log("Current temp: " + data.currently.temperature);
 			console.log("High temp: " + data.daily.data[0].temperatureMax);
 			console.log("Min temp: " + data.daily.data[0].temperatureMin);
 			console.log(data.daily.data[0].icon);
+			// append the humidity information as a percentage to the document
 			$("#weather-details .humidity").html("<p>Humidity: " + data.currently.humidity * 100 + "%</p>");
 			console.log("Humidity: " + data.currently.humidity);
+			// append the wind speen to the document
 			$("#weather-details .wind").html("<p>Wind: " + data.currently.windSpeed + " KPH</p>");
 			console.log("Wind speed: " + data.currently.windSpeed);
 
@@ -87,7 +98,7 @@ function getDarkskyData(url) {
 	});
 }
 
-
+// following function will display the appropriate clothing advice based on temperature and precipitation data
 function giveTip(temp, precip) {
 	console.log(temp, precip);
 
@@ -113,10 +124,9 @@ function giveTip(temp, precip) {
 	}
 	
 	$("#clothing-tips .tips").html("<p>" + tip + "</p>");
-
 }
 
-
+// change the background colour based on the general weather conditions
 function setBackground(icon) {
 	console.log(icon);
 	switch (icon) {
